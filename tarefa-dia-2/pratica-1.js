d9b43aab5f570013a3c453ec228ee2a7d953beb5
Guilherme Marins <106755788+marinsgui@users.noreply.github.com>
2022-08-25 17:01:47 -0300

    Update pratica-1.js

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -1,5 +1,7 @@
 let meuNome = 'Guilherme'
 let meuEmail = 'marins0guilherme@gmail.com'
 let profissao = 'Estudante de programação'
-let minhaIdade = '25 anos'
+let minhaIdade = 25
 let viagemDosSonhos = 'Japão'
+
+console.log(`Olá, ${meuNome}! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para ${viagemDosSonhos}. Confirma pra gente alguns dados? A sua idade é ${minhaIdade}, você é ${profissao} e o seu email é ${meuEmail}? Ficamos no aguardo. Muito obrigado e boa viagem! :D`)
